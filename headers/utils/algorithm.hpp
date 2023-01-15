301b49a0679fe281eeb619d94e30e770673323d0
kny-i <kny_i@keio.jp>
2023-01-15 19:00:10 +0900

    add eqall

---
+++ headers/utils/algorithm.hpp
@@ -0,0 +1,26 @@
+#ifndef ALGORITHM_HPP
+#define ALGORITHM_HPP
+
+namespace ft {
+
+	template<class Inputiterator1, class Inputiterator2>
+	bool eqall(Inputiterator1 first, Inputiterator1 last, Inputiterator2 inputiterator2) {
+		for (; first != last; ++first) {
+			if (*first != *inputiterator2)
+				return false;
+
+		}
+		return true;
+	}
+
+	template<class Inputiterator1, class Inputiterator2, class BinaryPredicate>
+	bool eqall(Inputiterator1 first, Inputiterator1 last, Inputiterator2 inputiterator2, BinaryPredicate predicate) {
+		for (; first != last; ++first) {
+			if (*first != *inputiterator2)
+				return false;
+
+		}
+		return true;
+	}
+};
+#endif
