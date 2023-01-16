58588c2c7104bcf1b045b506f93db13b552b090a
kny-i <kny_i@keio.jp>
2023-01-16 17:13:01 +0900

    wip

---
+++ headers/utils/avltree.hpp
@@ -0,0 +1,53 @@
+#ifndef AVL_TREE
+#define AVL_TREE
+
+#include <libc.h>
+
+
+struct node_t {
+	int num;
+	char name[1000];
+	struct node_t *left;
+	struct node_t *right;
+}		node_s;
+
+
+int getHight(node_t *node)
+{
+	int left_hight;
+	int right_hight;
+	int tree_hight;
+
+	if (node == NULL){
+		return 0;
+	}
+
+	left_hight = getHight(node->left);
+	right_hight = getHight(node->right);
+
+
+	if (left_hight > right_hight) {
+		tree_hight = left_hight;
+	} else {
+		tree_hight = right_hight;
+	}
+	return tree_hight + 1;
+}
+
+node_t *leftRotate(node_t *root, node_t *node, node_t *parent, int direction)
+{
+	node_t *pivot;
+	node_t *new_root;
+
+	printf("left_rotate[%d]\n", node->num);
+
+	pivot = node->right;
+	if (pivot != NULL) {
+
+	}
+	if (pivot == NULL) {
+		new_root = pivot;
+		return new_root;
+	}
+}
+#endif
