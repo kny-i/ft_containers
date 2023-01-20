5492291dd5dfb6dd275e21196d319daef0392fe7
kny-i <kny_i@keio.jp>
2023-01-21 02:32:35 +0900

    stack

--- headers/stack.hpp
+++ headers/stack.hpp
@@ -10,9 +10,43 @@ namespace ft {
 		typedef T value_type;
 		typedef Container container_type;
 		typedef size_t size_type;
+		explicit stack(const container_type &cont = container_type()) : c(cont) {}
 
-		explicit stack(const container_type &);
+		stack(const stack &other) : c(other.c) {}
+		~stack() {}
+
+		bool empty() const {return c.empty();}
+		size_type size() const {return c.size();}
+		value_type &top(){return c.back();}
+		const value_type &top() const{return c.back();}
+		void push(const value_type &value) {c.push_back(value);}
+		void pop() {return c.popback();}
+
+		friend bool operator==(const stack<T, Container> &lhs, const stack<T, Container> const &rhs) {
+			return lhs.c == rhs.c;
+		}
+		friend bool operator!=(const stack<T, Container> &lhs, const stack<T, Container> &rhs) {
+			return !(lhs.c == rhs.c);
+		}
+		friend bool operator<(const stack<T, Container> &lhs, const stack<T, Container> &rhs) {
+			return lhs.c < rhs.c;
+		}
+		friend bool operator<=(const stack<T, Container> &lhs, const stack<T, Container> &rhs) {
+			return !(rhs.c < lhs.c);
+		}
+		friend bool operator>(const stack<T, Container> &lhs, const stack<T, Container> &rhs) {
+			return rhs.c < lhs.c;
+		}
+		friend bool operator>=(const stack<T, Container> &lhs, const stack<T, Container> &rhs) {
+			return !(lhs.c < rhs.c);
+		}
+
+	protected:
+		Container c;
+
 	};
+
+
 
 }
 
