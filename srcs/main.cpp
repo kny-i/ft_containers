ca7ec3aed8cd10556d44429069f835d35e96bc78
kny-i <kny_i@keio.jp>
2023-01-24 02:22:24 +0900

    wip

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -7,5 +7,18 @@ int main()
 
 	ft::vector<int> a(1, 3);
 
+	//push_back
+	a.push_back(2);
+	a.push_back(2);
+	a.push_back(2);
+	a.push_back(2);
+
+	for (ft::vector<int>::iterator start = a.begin(); start != a.end(); ++start)
+	{
+		std::cout << *start << std::endl;
+	}
+
+
+
 }
 
