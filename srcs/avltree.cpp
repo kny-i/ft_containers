bde361babba9b2e2f2001786fad6e0afbee9255c
kny-i <kny_i@keio.jp>
2023-01-25 02:20:49 +0900

    wip

--- srcs/avltree.cpp
+++ srcs/avltree.cpp
@@ -4,8 +4,8 @@
 #include "tree_iterator.hpp"
 
 
-int	main()
-{
-
-	ft::avltree<int, ft::pair<int , std::string> > a;
-}
+//int	main()
+//{
+//
+//	ft::avltree<int, ft::pair<int , std::string> > a;
+//}
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,26 +1,41 @@
 #include <vector.hpp>
 
-//int main()
-//{
-//	std::cout << "hello world" << std::endl;
-//
-//
-//	ft::vector<int> a(1, 3);
-//
-	//push_back
-//	a.push_back(2);
-//	a.push_back(2);
-//	a.push_back(2);
-//	a.push_back(2);
-//
-//	for (ft::vector<int>::iterator start = a.begin(); start != a.end(); ++start)
-//	{
-//		std::cout << *start << std::endl;
-//	}
-//
-//
-//
-//
-//}
-//
+#include <pair.hpp>
+#include <map>
+int	main()
+{
+	std::map<int , std::string> a;
+
+	std::pair<int, std::string> b(1, "hoge");
+	std::pair<int, std::string> c(2, "piyo");
+	a.insert(b);
+	std::pair<std::map<int, std::string>::iterator, bool > d = a.insert(c);
+	std::map<int, std::string>::iterator hoge = d.first;
+	std::cout << hoge->second << std::endl;
+
+}
+
+/*
+
+int main()
+{
+	std::cout << "hello world" << std::endl;
+
+
+	ft::vector<int> a(1, 3);
+
+//	push_back
+	a.push_back(2);
+	a.push_back(2);
+	a.push_back(2);
+	a.push_back(2);
+
+	for (ft::vector<int>::iterator start = a.begin(); start != a.end(); ++start)
+	{
+		std::cout << *start << std::endl;
+	}
+
+}
+
+*/
 
