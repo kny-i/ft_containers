25f1859f44f8cf483c6f81138ffeb88855211445
kny-i <kny_i@keio.jp>
2023-01-25 07:34:03 +0900

    wip

---
+++ headers/utils/enableif.hpp
@@ -0,0 +1,13 @@
+#ifndef ENABLE
+#define ENABLE
+
+namespace ft {
+	template<bool B, typename T = void >
+	struct enable_if {
+		typedef T type;
+	};
+	template<typename T>
+	struct ft::enable_if<false, T> {};
+}
+
+#endif
