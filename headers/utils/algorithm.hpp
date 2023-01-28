6f9c8dfa4556b618f0daed1448a31b35b2bbe5b8
kny-i <kny_i@keio.jp>
2023-01-28 23:14:44 +0900

    wip

--- headers/utils/algorithm.hpp
+++ headers/utils/algorithm.hpp
@@ -22,10 +22,31 @@ namespace ft {
 		return true;
 	}
 
-	template <class Inputrator1, class Inputrator2>
-			bool lexicographical_compare(Inputrator1 first1, Inputrator1 last1, Inputrator2 first2, Inputrator2 last2) {
-//				for ()
-					;
-			}
+
+	template <class InputIterator1, class InputIterator2>
+	bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
+								 InputIterator2 first2, InputIterator2 last2) {
+		for (; first1 != last1 && first2 != last2; ++first1, ++first2) {
+			if (*first1 < *first2)
+				return true;
+			if (*first2 < *first1)
+				return false;
+		}
+		return first1 == last1 && first2 != last2;
+	}
+
+	template <class InputIterator1, class InputIterator2, class Compare>
+	bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
+								 InputIterator2 first2, InputIterator2 last2,
+								 Compare comp) {
+		for (; first1 != last1 && first2 != last2; ++first1, ++first2) {
+			if (comp(*first1, *first2))
+				return true;
+			if (comp(*first2, *first1))
+				return false;
+		}
+		return first1 == last1 && first2 != last2;
+	}
+
 };
 #endif
